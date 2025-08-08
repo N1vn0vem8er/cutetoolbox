6afaf948a1c8b0da482c522bca6c47fcd45a37ab
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-08-08 12:35:39 +0200

    Implemented time zones

--- src/widgets/timestampwidget.cpp
+++ src/widgets/timestampwidget.cpp
@@ -19,6 +19,13 @@ TimestampWidget::TimestampWidget(QWidget *parent)
     connect(ui->copyLongTimeButton, &QPushButton::clicked, this, [&]{ui->longTime->selectAll(); ui->longTime->copy();});
     connect(ui->copyFullDateButton, &QPushButton::clicked, this, [&]{ui->fullTimeDate->selectAll(); ui->fullTimeDate->copy();});
     connect(ui->copyCurrentTimeButton, &QPushButton::clicked, this, [&]{ui->currentTime->selectAll(); ui->currentTime->copy();});
+    const QList<QByteArray> timeZones = QTimeZone::availableTimeZoneIds();
+    for(const auto& i : timeZones)
+    {
+        ui->comboBox->addItem(i);
+    }
+    ui->comboBox->setCurrentText(QTimeZone::systemTimeZoneId());
+    connect(ui->comboBox, &QComboBox::currentIndexChanged, this, &TimestampWidget::setDateTime);
     updateTimestamp();
     setDateTime();
 }
@@ -37,11 +44,11 @@ void TimestampWidget::updateTimestamp()
 
 void TimestampWidget::setDateTime()
 {
-    ui->iso8601->setText(ui->dateTimeEdit->dateTime().toString(Qt::ISODate));
-    ui->rfc2822->setText(ui->dateTimeEdit->dateTime().toString(Qt::RFC2822Date));
-    ui->shortDate->setText(ui->dateTimeEdit->date().toString("dd:MM:yy"));
-    ui->shortTime->setText(ui->dateTimeEdit->time().toString("hh:mm:ss"));
-    ui->longDate->setText(ui->dateTimeEdit->date().toString("MMMM dd yy"));
-    ui->longTime->setText(ui->dateTimeEdit->time().toString("hh:mm:ss"));
-    ui->fullTimeDate->setText(ui->dateTimeEdit->dateTime().toUTC().toString("dddd, MMMM, dd yy hh:mm:ss"));
+    ui->iso8601->setText(ui->dateTimeEdit->dateTime().toTimeZone(QTimeZone(ui->comboBox->currentText().toUtf8())).toString(Qt::ISODate));
+    ui->rfc2822->setText(ui->dateTimeEdit->dateTime().toTimeZone(QTimeZone(ui->comboBox->currentText().toUtf8())).toString(Qt::RFC2822Date));
+    ui->shortDate->setText(ui->dateTimeEdit->dateTime().toTimeZone(QTimeZone(ui->comboBox->currentText().toUtf8())).toString("dd:MM:yy"));
+    ui->shortTime->setText(ui->dateTimeEdit->dateTime().toTimeZone(QTimeZone(ui->comboBox->currentText().toUtf8())).toString("hh:mm:ss"));
+    ui->longDate->setText(ui->dateTimeEdit->dateTime().toTimeZone(QTimeZone(ui->comboBox->currentText().toUtf8())).toString("MMMM dd yy"));
+    ui->longTime->setText(ui->dateTimeEdit->dateTime().toTimeZone(QTimeZone(ui->comboBox->currentText().toUtf8())).toString("hh:mm:ss"));
+    ui->fullTimeDate->setText(ui->dateTimeEdit->dateTime().toTimeZone(QTimeZone(ui->comboBox->currentText().toUtf8())).toString("dddd, MMMM, dd yy hh:mm:ss t"));
 }
