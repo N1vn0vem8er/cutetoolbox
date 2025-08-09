9ba1b5c6ddaafdc94f0f37803121fb24fed8be53
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-08-09 14:40:35 +0200

    Implemented qdatetimeconverterwidget

--- src/widgets/qdatetimeconverterwidget.cpp
+++ src/widgets/qdatetimeconverterwidget.cpp
@@ -6,9 +6,19 @@ QDateTimeConverterWidget::QDateTimeConverterWidget(QWidget *parent)
     , ui(new Ui::QDateTimeConverterWidget)
 {
     ui->setupUi(this);
+    ui->dateTimeEdit->setDateTime(QDateTime::currentDateTime());
+    connect(ui->format, &QLineEdit::textChanged, this, &QDateTimeConverterWidget::format);
+    connect(ui->dateTimeEdit, &QDateTimeEdit::dateTimeChanged, this, &QDateTimeConverterWidget::format);
+    connect(ui->getCurrentDateTimeButton, &QPushButton::clicked, this, [&]{ui->dateTimeEdit->setDateTime(QDateTime::currentDateTime());});
+    format();
 }
 
 QDateTimeConverterWidget::~QDateTimeConverterWidget()
 {
     delete ui;
+}
+
+void QDateTimeConverterWidget::format()
+{
+    ui->dateTime->setText(ui->dateTimeEdit->dateTime().toString(ui->format->text()));
 }
--- src/widgets/qdatetimeconverterwidget.h
+++ src/widgets/qdatetimeconverterwidget.h
@@ -17,6 +17,9 @@ public:
 
 private:
     Ui::QDateTimeConverterWidget *ui;
+
+private slots:
+    void format();
 };
 
 #endif // QDATETIMECONVERTERWIDGET_H
