04179eaaa895092b2c4068c53e314ef525e014bd
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-08-31 19:11:05 +0200

    Added open and save as to base64coderdecoderwidget

--- src/widgets/base64coderdecoderwidget.cpp
+++ src/widgets/base64coderdecoderwidget.cpp
@@ -49,6 +49,156 @@ Base64CoderDecoderWidget::~Base64CoderDecoderWidget()
     delete ui;
 }
 
+bool Base64CoderDecoderWidget::canOpenFiles() const
+{
+    return true;
+}
+
+bool Base64CoderDecoderWidget::canSaveFiles() const
+{
+    return true;
+}
+
+bool Base64CoderDecoderWidget::canBasicEdit() const
+{
+    return true;
+}
+
+bool Base64CoderDecoderWidget::canChangeFont() const
+{
+    return true;
+}
+
+void Base64CoderDecoderWidget::save()
+{
+
+}
+
+void Base64CoderDecoderWidget::saveAs()
+{
+    TextEdits option = getSelectedOption();
+    if(option != TextEdits::none)
+    {
+        const QString path = QFileDialog::getSaveFileName(this, tr("Save As"), QDir::homePath());
+        if(!path.isEmpty())
+        {
+            QFile file(path);
+            if(file.open(QIODevice::WriteOnly))
+            {
+                file.write(option == TextEdits::html ? ui->text->toPlainText().toUtf8() : ui->base64->toPlainText().toUtf8());
+                file.close();
+            }
+        }
+    }
+}
+
+void Base64CoderDecoderWidget::open()
+{
+    TextEdits option = getSelectedOption();
+    if(option != TextEdits::none)
+    {
+        const QString path = QFileDialog::getOpenFileName(this, tr("Open"), QDir::homePath());
+        if(!path.isEmpty())
+        {
+            QFile file(path);
+            if(file.open(QIODevice::ReadOnly))
+            {
+                if(option == TextEdits::html)
+                    ui->text->setPlainText(file.readAll());
+                else if(option == TextEdits::encoded)
+                    ui->base64->setPlainText(file.readAll());
+                file.close();
+            }
+        }
+    }
+}
+
+void Base64CoderDecoderWidget::copy()
+{
+
+}
+
+void Base64CoderDecoderWidget::cut()
+{
+
+}
+
+void Base64CoderDecoderWidget::paste()
+{
+
+}
+
+void Base64CoderDecoderWidget::selectAll()
+{
+
+}
+
+void Base64CoderDecoderWidget::deleteText()
+{
+
+}
+
+void Base64CoderDecoderWidget::deleteAllText()
+{
+
+}
+
+void Base64CoderDecoderWidget::undo()
+{
+
+}
+
+void Base64CoderDecoderWidget::redo()
+{
+
+}
+
+void Base64CoderDecoderWidget::increaseFontSize()
+{
+
+}
+
+void Base64CoderDecoderWidget::decreaseFontSize()
+{
+
+}
+
+void Base64CoderDecoderWidget::setFontSize()
+{
+
+}
+
+void Base64CoderDecoderWidget::resetFontSize()
+{
+
+}
+
+void Base64CoderDecoderWidget::setFont()
+{
+
+}
+
+Base64CoderDecoderWidget::TextEdits Base64CoderDecoderWidget::getSelectedOption()
+{
+    TextEdits option = TextEdits::none;
+    QDialog dialog(this);
+    QHBoxLayout layout(&dialog);
+    QPushButton htmlButton(tr("Text"), &dialog);
+    QPushButton encodedButton(tr("Base64"), &dialog);
+    connect(&htmlButton, &QPushButton::clicked, &dialog, [&]{option = TextEdits::html;});
+    connect(&encodedButton, &QPushButton::clicked, &dialog, [&]{option = TextEdits::encoded;});
+    connect(&htmlButton, &QPushButton::clicked, &dialog, &QDialog::accept);
+    connect(&encodedButton, &QPushButton::clicked, &dialog, &QDialog::accept);
+    layout.addWidget(&htmlButton);
+    layout.addWidget(&encodedButton);
+    dialog.setLayout(&layout);
+    if(dialog.exec() == QDialog::Accepted)
+    {
+        return option;
+    }
+    return option;
+}
+
 void Base64CoderDecoderWidget::encode()
 {
     if(!coding)
--- src/widgets/base64coderdecoderwidget.h
+++ src/widgets/base64coderdecoderwidget.h
@@ -15,10 +15,36 @@ class Base64CoderDecoderWidget : public CustomWidget
 public:
     explicit Base64CoderDecoderWidget(QWidget *parent = nullptr);
     ~Base64CoderDecoderWidget();
+    bool canOpenFiles() const override;
+    bool canSaveFiles() const override;
+    bool canBasicEdit() const override;
+    bool canChangeFont() const override;
+    void save() override;
+    void saveAs() override;
+    void open() override;
+    void copy() override;
+    void cut() override;
+    void paste() override;
+    void selectAll() override;
+    void deleteText() override;
+    void deleteAllText() override;
+    void undo() override;
+    void redo() override;
+    void increaseFontSize() override;
+    void decreaseFontSize() override;
+    void setFontSize() override;
+    void resetFontSize() override;
+    void setFont() override;
 
 private:
+    enum TextEdits{
+        html,
+        encoded,
+        none
+    };
     Ui::Base64CoderDecoderWidget *ui;
     bool coding {false};
+    TextEdits getSelectedOption();
 
 private slots:
     void encode();
