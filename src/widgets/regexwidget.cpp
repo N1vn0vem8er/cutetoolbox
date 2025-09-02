968d880a044d57cd964241e3b2e9ab9c032b864e
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-09-02 11:49:06 +0200

    Implemented actions for regexwidget

--- src/widgets/regexwidget.cpp
+++ src/widgets/regexwidget.cpp
@@ -1,28 +1,18 @@
 #include "regexwidget.h"
 #include "src/widgets/ui_regexwidget.h"
 #include <QFileDialog>
+#include <QFontDialog>
+#include <QInputDialog>
 #include <QRegularExpression>
 
 RegexWidget::RegexWidget(QWidget *parent)
-    : QWidget(parent)
+    : CustomWidget(parent)
     , ui(new Ui::RegexWidget)
 {
     ui->setupUi(this);
     connect(ui->lineEdit, &QLineEdit::textChanged, this, &RegexWidget::regex);
     connect(ui->plainTextEdit, &CodeEditor::textChanged, this, &RegexWidget::regex);
-    connect(ui->openTextButton, &QPushButton::clicked, this, [&]{
-        const QString path = QFileDialog::getOpenFileName(this, tr("Open file"), QDir::homePath());
-        if(!path.isEmpty())
-        {
-            QFile file(path);
-            file.open(QIODevice::ReadOnly);
-            if(file.isOpen())
-            {
-                ui->plainTextEdit->setPlainText(file.readAll());
-                file.close();
-            }
-        }
-    });
+    connect(ui->openTextButton, &QPushButton::clicked, this, &RegexWidget::open);
     connect(ui->copyRegexButton, &QPushButton::clicked, this, [&]{ui->lineEdit->selectAll(); ui->lineEdit->copy();});
     connect(ui->pasteRegexButton, &QPushButton::clicked, ui->lineEdit, &QLineEdit::paste);
     connect(ui->clearRegexButton, &QPushButton::clicked, ui->lineEdit, &QLineEdit::clear);
@@ -34,6 +24,69 @@ RegexWidget::RegexWidget(QWidget *parent)
 RegexWidget::~RegexWidget()
 {
     delete ui;
+}
+
+bool RegexWidget::canOpenFiles() const
+{
+    return true;
+}
+
+bool RegexWidget::canBasicEdit() const
+{
+    return true;
+}
+
+bool RegexWidget::canChangeFont() const
+{
+    return true;
+}
+
+void RegexWidget::open()
+{
+    const QString path = QFileDialog::getOpenFileName(this, tr("Open file"), QDir::homePath());
+    if(!path.isEmpty())
+    {
+        QFile file(path);
+        file.open(QIODevice::ReadOnly);
+        if(file.isOpen())
+        {
+            ui->plainTextEdit->setPlainText(file.readAll());
+            file.close();
+        }
+    }
+}
+
+void RegexWidget::increaseFontSize()
+{
+    ui->plainTextEdit->increaseFontSize();
+}
+
+void RegexWidget::decreaseFontSize()
+{
+    ui->plainTextEdit->decreaseFontSize();
+}
+
+void RegexWidget::setFontSize()
+{
+    bool ok;
+    const int size = QInputDialog::getInt(this, tr("Set font size"), tr("Font size"), 1, 1, 200, 1, &ok);
+    if(ok)
+        ui->plainTextEdit->setFontSize(size);
+}
+
+void RegexWidget::resetFontSize()
+{
+    ui->plainTextEdit->setFontSize(10);
+}
+
+void RegexWidget::setFont()
+{
+    bool ok;
+    const QFont font = QFontDialog::getFont(&ok, this);
+    if(ok)
+    {
+        ui->plainTextEdit->setFont(font);
+    }
 }
 
 void RegexWidget::regex()
--- src/widgets/regexwidget.h
+++ src/widgets/regexwidget.h
@@ -1,19 +1,29 @@
 #ifndef REGEXWIDGET_H
 #define REGEXWIDGET_H
 
+#include "customwidget.h"
 #include <QWidget>
 
 namespace Ui {
 class RegexWidget;
 }
 
-class RegexWidget : public QWidget
+class RegexWidget : public CustomWidget
 {
     Q_OBJECT
 
 public:
     explicit RegexWidget(QWidget *parent = nullptr);
     ~RegexWidget();
+    bool canOpenFiles() const override;
+    bool canBasicEdit() const override;
+    bool canChangeFont() const override;
+    void open() override;
+    void increaseFontSize() override;
+    void decreaseFontSize() override;
+    void setFontSize() override;
+    void resetFontSize() override;
+    void setFont() override;
 
 private:
     Ui::RegexWidget *ui;
