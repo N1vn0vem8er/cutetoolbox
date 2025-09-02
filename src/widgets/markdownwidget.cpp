9a8b0cb5bc3f30a1c50426db70be64112ae61026
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-09-02 12:46:47 +0200

    Implemented actions for markdownwidget

--- src/widgets/markdownwidget.cpp
+++ src/widgets/markdownwidget.cpp
@@ -2,13 +2,16 @@
 #include "previewpage.h"
 #include "src/widgets/ui_markdownwidget.h"
 #include "syntaxhighlighters/markdownsyntaxhighlighter.h"
+#include <QFileDialog>
+#include <QFontDialog>
+#include <QInputDialog>
 #include <QPrintDialog>
 #include <QPrinter>
 #include <QWebChannel>
 #include <qdir.h>
 
 MarkdownWidget::MarkdownWidget(QWidget *parent)
-    : QWidget(parent)
+    : CustomWidget(parent)
     , ui(new Ui::MarkdownWidget)
 {
     ui->setupUi(this);
@@ -26,4 +29,100 @@ MarkdownWidget::MarkdownWidget(QWidget *parent)
 MarkdownWidget::~MarkdownWidget()
 {
     delete ui;
+}
+
+bool MarkdownWidget::canOpenFiles() const
+{
+    return true;
+}
+
+bool MarkdownWidget::canSaveFiles() const
+{
+    return true;
+}
+
+bool MarkdownWidget::canBasicEdit() const
+{
+    return true;
+}
+
+bool MarkdownWidget::canChangeFont() const
+{
+    return true;
+}
+
+void MarkdownWidget::save()
+{
+    if(!openedFile.isEmpty())
+    {
+        QFile file(openedFile);
+        if(file.open(QIODevice::WriteOnly))
+        {
+            file.write(ui->editor->toPlainText().toUtf8());
+            file.close();
+        }
+    }
+    else
+        saveAs();
+}
+
+void MarkdownWidget::saveAs()
+{
+    const QString path = QFileDialog::getSaveFileName(this, tr("Save As"), QDir::homePath(), "*.md");
+    if(!path.isEmpty())
+    {
+        QFile file(path);
+        if(file.open(QIODevice::WriteOnly))
+        {
+            file.write(ui->editor->toPlainText().toUtf8());
+            file.close();
+            openedFile = path;
+        }
+    }
+}
+
+void MarkdownWidget::open()
+{
+    const QString path = QFileDialog::getOpenFileName(this, tr("Open"), QDir::homePath(), "*.md");
+    if(!path.isEmpty())
+    {
+        QFile file(path);
+        if(file.open(QIODevice::ReadOnly))
+        {
+            ui->editor->setPlainText(file.readAll());
+            file.close();
+            openedFile = path;
+        }
+    }
+}
+
+void MarkdownWidget::increaseFontSize()
+{
+    ui->editor->increaseFontSize();
+}
+
+void MarkdownWidget::decreaseFontSize()
+{
+    ui->editor->decreaseFontSize();
+}
+
+void MarkdownWidget::setFontSize()
+{
+    bool ok;
+    const int size = QInputDialog::getInt(this, tr("Set font size"), tr("Font size"), 1, 1, 200, 1, &ok);
+    if(ok)
+        ui->editor->setFontSize(size);
+}
+
+void MarkdownWidget::resetFontSize()
+{
+    ui->editor->setFontSize(10);
+}
+
+void MarkdownWidget::setFont()
+{
+    bool ok;
+    const QFont font = QFontDialog::getFont(&ok, this);
+    if(ok)
+        ui->editor->setFont(font);
 }
--- src/widgets/markdownwidget.h
+++ src/widgets/markdownwidget.h
@@ -1,6 +1,7 @@
 #ifndef MARKDOWNWIDGET_H
 #define MARKDOWNWIDGET_H
 
+#include "customwidget.h"
 #include "document.h"
 #include "syntaxhighlighters/markdownsyntaxhighlighter.h"
 #include <QWidget>
@@ -9,18 +10,31 @@ namespace Ui {
 class MarkdownWidget;
 }
 
-class MarkdownWidget : public QWidget
+class MarkdownWidget : public CustomWidget
 {
     Q_OBJECT
 
 public:
     explicit MarkdownWidget(QWidget *parent = nullptr);
     ~MarkdownWidget();
+    bool canOpenFiles() const override;
+    bool canSaveFiles() const override;
+    bool canBasicEdit() const override;
+    bool canChangeFont() const override;
+    void save() override;
+    void saveAs() override;
+    void open() override;
+    void increaseFontSize() override;
+    void decreaseFontSize() override;
+    void setFontSize() override;
+    void resetFontSize() override;
+    void setFont() override;
 
 private:
     Ui::MarkdownWidget *ui;
     Document document;
     MarkdownSyntaxHighlighter* syntaxHighlighter {nullptr};
+    QString openedFile;
 };
 
 #endif // MARKDOWNWIDGET_H
