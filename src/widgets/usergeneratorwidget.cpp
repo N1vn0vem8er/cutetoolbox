6291c8ec3a154253416b639e30c3963c8f4a4adb
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-09-14 14:52:10 +0200

    Implemented generating names, phone numbers and emails

--- src/widgets/usergeneratorwidget.cpp
+++ src/widgets/usergeneratorwidget.cpp
@@ -1,11 +1,15 @@
 #include "usergeneratorwidget.h"
 #include "src/widgets/ui_usergeneratorwidget.h"
+#include <QStandardItemModel>
+#include <qdialog.h>
+#include <random>
 
 UserGeneratorWidget::UserGeneratorWidget(QWidget *parent)
     : CustomWidget(parent)
     , ui(new Ui::UserGeneratorWidget)
 {
     ui->setupUi(this);
+    connect(ui->generateButton, &QPushButton::clicked, this, &UserGeneratorWidget::generate);
 }
 
 UserGeneratorWidget::~UserGeneratorWidget()
@@ -33,7 +37,81 @@ void UserGeneratorWidget::saveAs()
 
 }
 
+QString UserGeneratorWidget::getRandomQString(const QStringList &list) const
+{
+    std::random_device rg;
+    std::mt19937_64 generator(rg());
+    std::uniform_int_distribution<int> random(0, list.length() - 1);
+    return list[random(generator)];
+}
+
 void UserGeneratorWidget::generate()
 {
-
+    if(ui->tableView->model())
+        ui->tableView->model()->deleteLater();
+    QStandardItemModel* model = new QStandardItemModel(ui->tableView);
+    std::random_device rg;
+    std::mt19937_64 generator(rg());
+    std::uniform_int_distribution<int> random(0, 9);
+    int index = 0;
+    if(ui->firstNameCheckBox->isChecked())
+    {
+        model->setHorizontalHeaderItem(index++, new QStandardItem("First Name"));
+    }
+    if(ui->lastNameCheckBox->isChecked())
+    {
+        model->setHorizontalHeaderItem(index++, new QStandardItem("Last Name"));
+    }
+    if(ui->emailCheckBox->isChecked())
+    {
+        model->setHorizontalHeaderItem(index++, new QStandardItem("Email"));
+    }
+    if(ui->phoneNumberCheckBox->isChecked())
+    {
+        model->setHorizontalHeaderItem(index++, new QStandardItem("Phone Number"));
+    }
+    for(int i = 0; i < ui->quantitySpinBox->value(); i++)
+    {
+        QList<QStandardItem*> row;
+        if(ui->firstNameCheckBox->isChecked())
+            row.append(new QStandardItem(getRandomQString(firstNames)));
+        if(ui->lastNameCheckBox->isChecked())
+            row.append(new QStandardItem(getRandomQString(lastNames)));
+        if(ui->emailCheckBox->isChecked())
+        {
+            QString email;
+            if(ui->firstNameCheckBox->isChecked())
+            {
+                email += row[0]->text();
+                if(ui->lastNameCheckBox->isChecked())
+                    email += row[1]->text();
+                else
+                    email += getRandomQString(lastNames);
+            }
+            else if(ui->lastNameCheckBox->isChecked())
+            {
+                email += getRandomQString(firstNames);
+                email += row[0]->text();
+            }
+            else
+            {
+                email += getRandomQString(firstNames) + getRandomQString(lastNames);
+            }
+            for(int i=0; i < 5; i++)
+                email += QString::number(random(generator));
+            email += "@example.com";
+            row.append(new QStandardItem(email));
+        }
+        if(ui->phoneNumberCheckBox->isChecked())
+        {
+            QString number;
+            for(int i=0; i<ui->phoneLength->value(); i++)
+            {
+                number += QString::number(random(generator));
+            }
+            row.append(new QStandardItem(number));
+        }
+        model->insertRow(i, row);
+    }
+    ui->tableView->setModel(model);
 }
--- src/widgets/usergeneratorwidget.h
+++ src/widgets/usergeneratorwidget.h
@@ -3,6 +3,8 @@
 
 #include "customwidget.h"
 #include <QWidget>
+#include <qboxlayout.h>
+#include <qspinbox.h>
 
 namespace Ui {
 class UserGeneratorWidget;
@@ -23,8 +25,29 @@ public:
 private:
     Ui::UserGeneratorWidget *ui;
 
+private:
+    QString getRandomQString(const QStringList& list) const;
+
 private slots:
     void generate();
+
+private:
+    const QStringList firstNames {
+        "Olivia", "Emma", "Charlotte", "Amelia", "Sophia", "Mia", "Isabella", "Ava", "Evelyn", "Luna", "Harper", "Camila", "Gianna", "Elizabeth", "Ellie", "Sofia", "Ella", "Penelope", "Nova",
+        "Aurora", "Scarlett", "Mila", "Chloe", "Layla", "Zoe", "Nora", "Lily", "Iris", "Paisley", "Violet", "Willow", "Grace", "Stella", "Naomi", "Savannah", "Aria", "Everly", "Lillian", "Madison",
+        "Natalie", "Hazel", "Brooklyn", "Aaliyah", "Audrey", "Hannah", "Zoey", "Alice", "Elena", "Maya", "Victoria", "Liam", "Noah", "Oliver", "James", "Elijah", "William", "Henry", "Lucas",
+        "Benjamin", "Theodore", "Mateo", "Levi", "Sebastian", "Jack", "Ethan", "Mason", "Luke", "Ezra", "Asher", "Grayson", "Leo", "Samuel", "Daniel", "Aiden", "Joseph", "Owen", "Wyatt", "Caleb",
+        "Hudson", "Lincoln", "Jayden", "Jackson", "Julian", "Gabriel", "Alexander", "Elias", "Maverick", "Michael", "Isaiah", "Nicholas", "David", "Miles", "Jeremiah", "John", "Nathan", "Adrian",
+        "Angel", "Christopher", "Jaxon", "Jonathan"
+    };
+    const QStringList lastNames {
+        "Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson", "Thomas", "Taylor", "Moore",
+        "Jackson", "Martin", "Lee", "Perez", "Thompson", "White", "Harris", "Sanchez", "Clark", "Ramirez", "Lewis", "Robinson", "Walker", "Hall", "Young", "Allen", "King", "Wright", "Scott", "Torres",
+        "Nguyen", "Hill", "Flores", "Green", "Adams", "Nelson", "Baker", "Hall", "Rivera", "Campbell", "Mitchell", "Carter", "Roberts", "Gomez", "Phillips", "Evans", "Turner", "Diaz", "Parker", "Cruz",
+        "Edwards", "Collins", "Reyes", "Stewart", "Morris", "Morales", "Murphy", "Cook", "Rogers", "Gupta", "Morgan", "Peterson", "Cooper", "Reed", "Bailey", "Bell", "Coleman", "Howard", "Ward", "Foster",
+        "Gonzales", "Watson", "Bryant", "Russell", "Patterson", "Gray", "Hughes", "James", "Washington", "Simmons", "Bennett", "Richardson", "Cox", "Alexander", "Wood", "Coleman", "Kelly", "Henderson",
+        "Chavez", "Diaz"
+    };
 
 };
 
