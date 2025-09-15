28abd455a274e598984d44d444cda4e7e693331d
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-09-15 13:19:45 +0200

    Implemented formatting

--- src/widgets/cppformatterwidget.cpp
+++ src/widgets/cppformatterwidget.cpp
@@ -1,12 +1,14 @@
 #include "cppformatterwidget.h"
 #include "src/widget/ui_cppformatterwidget.h"
 
+#include <QProcess>
+
 CppFormatterWidget::CppFormatterWidget(QWidget *parent)
     : CustomWidget(parent)
     , ui(new Ui::CppFormatterWidget)
 {
     ui->setupUi(this);
-
+    connect(ui->formatButton, &QPushButton::clicked, this, &CppFormatterWidget::format);
 }
 
 CppFormatterWidget::~CppFormatterWidget()
@@ -76,5 +78,12 @@ void CppFormatterWidget::setFont()
 
 void CppFormatterWidget::format()
 {
-
+    QProcess* process = new QProcess(this);
+    connect(process, &QProcess::readyReadStandardOutput, this, [this, process]{ui->codeEditor->setPlainText(process->readAllStandardOutput());});
+    connect(process, &QProcess::readyReadStandardError, this, [this, process]{qDebug() << process->readAllStandardError();});
+    connect(process, &QProcess::finished, process, &QProcess::deleteLater);
+    process->start("clang-format", {QString("--style=%1").arg(ui->styleComboBox->currentText()), "--assume-filename=some_file.cpp"});
+    process->waitForStarted();
+    process->write(ui->codeEditor->toPlainText().toUtf8());
+    process->closeWriteChannel();
 }
