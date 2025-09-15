ecacd5e5266c8cb675414545aab8484b123c1da8
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-09-15 18:03:08 +0200

    Implemented opening and saving files

--- src/widgets/javaformatterwidget.cpp
+++ src/widgets/javaformatterwidget.cpp
@@ -1,6 +1,11 @@
 #include "javaformatterwidget.h"
 #include "src/widgets/ui_javaformatterwidget.h"
 
+#include <QFileDialog>
+#include <QFontDialog>
+#include <QInputDialog>
+#include <QProcess>
+
 JavaFormatterWidget::JavaFormatterWidget(QWidget *parent)
     : CustomWidget(parent)
     , ui(new Ui::JavaFormatterWidget)
@@ -11,4 +16,112 @@ JavaFormatterWidget::JavaFormatterWidget(QWidget *parent)
 JavaFormatterWidget::~JavaFormatterWidget()
 {
     delete ui;
+}
+
+bool JavaFormatterWidget::canOpenFiles() const
+{
+    return true;
+}
+
+bool JavaFormatterWidget::canSaveFiles() const
+{
+    return true;
+}
+
+bool JavaFormatterWidget::canBasicEdit() const
+{
+    return true;
+}
+
+bool JavaFormatterWidget::canChangeFont() const
+{
+    return true;
+}
+
+void JavaFormatterWidget::save()
+{
+    if(!openedFile.isEmpty())
+    {
+        QFile file(openedFile);
+        if(file.open(QIODevice::WriteOnly))
+        {
+            file.write(ui->codeEditor->toPlainText().toUtf8());
+            file.close();
+        }
+    }
+    else
+        saveAs();
+}
+
+void JavaFormatterWidget::saveAs()
+{
+    const QString path = QFileDialog::getSaveFileName(this, tr("Save As"), QDir::homePath(), "*.cs");
+    if(!path.isEmpty())
+    {
+        QFile file(path);
+        if(file.open(QIODevice::WriteOnly))
+        {
+            file.write(ui->codeEditor->toPlainText().toUtf8());
+            file.close();
+            openedFile = path;
+        }
+    }
+}
+
+void JavaFormatterWidget::open()
+{
+    const QString path = QFileDialog::getOpenFileName(this, tr("Open"), QDir::homePath(), "*.cs");
+    if(!path.isEmpty())
+    {
+        QFile file(path);
+        if(file.open(QIODevice::ReadOnly))
+        {
+            ui->codeEditor->setPlainText(file.readAll());
+            file.close();
+            openedFile = path;
+        }
+    }
+}
+
+void JavaFormatterWidget::increaseFontSize()
+{
+    ui->codeEditor->increaseFontSize();
+}
+
+void JavaFormatterWidget::decreaseFontSize()
+{
+    ui->codeEditor->decreaseFontSize();
+}
+
+void JavaFormatterWidget::setFontSize()
+{
+    bool ok;
+    const int size = QInputDialog::getInt(this, tr("Set font size"), tr("Font size"), 1, 1, 200, 1, &ok);
+    if(ok)
+        ui->codeEditor->setFontSize(size);
+}
+
+void JavaFormatterWidget::resetFontSize()
+{
+    ui->codeEditor->setFontSize(10);
+}
+
+void JavaFormatterWidget::setFont()
+{
+    bool ok;
+    const QFont font = QFontDialog::getFont(&ok, this);
+    if(ok)
+        ui->codeEditor->setFont(font);
+}
+
+void JavaFormatterWidget::format()
+{
+    QProcess* process = new QProcess(this);
+    connect(process, &QProcess::readyReadStandardOutput, this, [this, process]{ui->codeEditor->setPlainText(process->readAllStandardOutput());});
+    connect(process, &QProcess::readyReadStandardError, this, [this, process]{qDebug() << process->readAllStandardError();});
+    connect(process, &QProcess::finished, process, &QProcess::deleteLater);
+    process->start("clang-format", {QString("--style=%1").arg(ui->styleComboBox->currentText()), "--assume-filename=some_file.java"});
+    process->waitForStarted();
+    process->write(ui->codeEditor->toPlainText().toUtf8());
+    process->closeWriteChannel();
 }
--- src/widgets/javaformatterwidget.h
+++ src/widgets/javaformatterwidget.h
@@ -15,9 +15,25 @@ class JavaFormatterWidget : public CustomWidget
 public:
     explicit JavaFormatterWidget(QWidget *parent = nullptr);
     ~JavaFormatterWidget();
+    bool canOpenFiles() const override;
+    bool canSaveFiles() const override;
+    bool canBasicEdit() const override;
+    bool canChangeFont() const override;
+    void save() override;
+    void saveAs() override;
+    void open() override;
+    void increaseFontSize() override;
+    void decreaseFontSize() override;
+    void setFontSize() override;
+    void resetFontSize() override;
+    void setFont() override;
 
 private:
     Ui::JavaFormatterWidget *ui;
+    QString openedFile;
+
+private slots:
+    void format();
 };
 
 #endif // JAVAFORMATTERWIDGET_H
