862a24773df4113fc23d2636347a0ee0b8730bfa
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-10-27 11:08:21 +0100

    Implemented recent files for objectivecformatterwidget

--- src/widgets/objectivecformatterwidget.cpp
+++ src/widgets/objectivecformatterwidget.cpp
@@ -20,12 +20,28 @@ ObjectiveCFormatterWidget::ObjectiveCFormatterWidget(QWidget *parent)
     connect(ui->copyButton, &QPushButton::clicked, ui->codeEditor, &CodeEditor::copyAll);
     connect(ui->pasteButton, &QPushButton::clicked, ui->codeEditor, &CodeEditor::paste);
     connect(ui->clearButton, &QPushButton::clicked, ui->codeEditor, &CodeEditor::clear);
+    int size = settings.beginReadArray("objectivecformatter.recentFiles");
+    for(int i = 0; i<size; i++)
+    {
+        settings.setArrayIndex(i);
+        const QString path = settings.value("path").toString();
+        if(!path.isEmpty())
+            recentFiles.append(path);
+    }
+    settings.endArray();
 }
 
 ObjectiveCFormatterWidget::~ObjectiveCFormatterWidget()
 {
     QSettings settings(Config::settingsName);
     settings.setValue("objectivecformatter.style", ui->styleComboBox->currentIndex());
+    settings.beginWriteArray("objectivecformatter.recentFiles");
+    for(int i = 0; i<recentFiles.size(); i++)
+    {
+        settings.setArrayIndex(i);
+        settings.setValue("path", recentFiles.at(i));
+    }
+    settings.endArray();
     delete ui;
 }
 
@@ -93,6 +109,11 @@ void ObjectiveCFormatterWidget::open()
             ui->codeEditor->setPlainText(file.readAll());
             file.close();
             openedFile = path;
+            if(recentFiles.length() >= 10)
+                recentFiles.removeFirst();
+            if(!recentFiles.contains(openedFile))
+                recentFiles.append(openedFile);
+            emit updateRecent();
             emit opened(openedFile);
         }
     }
@@ -138,6 +159,32 @@ void ObjectiveCFormatterWidget::setFont()
 QString ObjectiveCFormatterWidget::getOpenedFileName() const
 {
     return openedFile;
+}
+
+QStringList ObjectiveCFormatterWidget::getRecentFiles() const
+{
+    return recentFiles;
+}
+
+void ObjectiveCFormatterWidget::openFromRecent(const QString &path)
+{
+    if(recentFiles.contains(path))
+    {
+        QFile file(path);
+        if(file.open(QIODevice::ReadOnly))
+        {
+            ui->codeEditor->setPlainText(file.readAll());
+            file.close();
+            openedFile = path;
+            emit opened(openedFile);
+        }
+    }
+}
+
+void ObjectiveCFormatterWidget::clearRecent()
+{
+    recentFiles.clear();
+    emit updateRecent();
 }
 
 void ObjectiveCFormatterWidget::format()
