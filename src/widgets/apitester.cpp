865b6b1c2ec113ca651db5cfb4d5021324b6b48a
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-11-07 16:38:57 +0100

    Added networking

--- src/widgets/apitester.cpp
+++ src/widgets/apitester.cpp
@@ -6,9 +6,117 @@ ApiTester::ApiTester(QWidget *parent)
     , ui(new Ui::ApiTester)
 {
     ui->setupUi(this);
+    setName(tr("Api Tester"));
+    networkManager = new QNetworkAccessManager(this);
+    connect(networkManager, &QNetworkAccessManager::finished, this, &ApiTester::onRequestFinished);
 }
 
 ApiTester::~ApiTester()
 {
     delete ui;
+}
+
+bool ApiTester::canOpenFiles() const
+{
+    return true;
+}
+
+bool ApiTester::canSaveFiles() const
+{
+    return true;
+}
+
+bool ApiTester::canBasicEdit() const
+{
+    return true;
+}
+
+bool ApiTester::canChangeFont() const
+{
+    return true;
+}
+
+void ApiTester::save()
+{
+
+}
+
+void ApiTester::saveAs()
+{
+
+}
+
+void ApiTester::open()
+{
+
+}
+
+void ApiTester::close()
+{
+
+}
+
+void ApiTester::increaseFontSize()
+{
+
+}
+
+void ApiTester::decreaseFontSize()
+{
+
+}
+
+void ApiTester::setFontSize()
+{
+
+}
+
+void ApiTester::resetFontSize()
+{
+
+}
+
+void ApiTester::setFont()
+{
+
+}
+
+QString ApiTester::getOpenedFileName() const
+{
+    return openedRequestFile + " " + openedResponseFile;
+}
+
+QStringList ApiTester::getRecentFiles() const
+{
+    return recentRequestFiles + recentResponseFiles;
+}
+
+void ApiTester::openFromRecent(const QString &path)
+{
+
+}
+
+void ApiTester::clearRecent()
+{
+
+}
+
+void ApiTester::sendGetRequest()
+{
+
+}
+
+void ApiTester::sendPostRequest()
+{
+
+}
+
+void ApiTester::sendPutRequest()
+{
+
+}
+
+void ApiTester::onRequestFinished(QNetworkReply *reply)
+{
+
 }
--- src/widgets/apitester.h
+++ src/widgets/apitester.h
@@ -3,6 +3,7 @@
 
 #include "customwidget.h"
 #include <QWidget>
+#include <QNetworkAccessManager>
 
 namespace Ui {
 class ApiTester;
@@ -15,9 +16,37 @@ class ApiTester : public CustomWidget
 public:
     explicit ApiTester(QWidget *parent = nullptr);
     ~ApiTester();
+    bool canOpenFiles() const override;
+    bool canSaveFiles() const override;
+    bool canBasicEdit() const override;
+    bool canChangeFont() const override;
+    void save() override;
+    void saveAs() override;
+    void open() override;
+    void close() override;
+    void increaseFontSize() override;
+    void decreaseFontSize() override;
+    void setFontSize() override;
+    void resetFontSize() override;
+    void setFont() override;
+    QString getOpenedFileName() const override;
+    QStringList getRecentFiles() const override;
+    void openFromRecent(const QString &path) override;
+    void clearRecent() override;
 
 private:
     Ui::ApiTester *ui;
+    QString openedRequestFile;
+    QString openedResponseFile;
+    QStringList recentRequestFiles;
+    QStringList recentResponseFiles;
+    QNetworkAccessManager* networkManager {nullptr};
+
+private slots:
+    void sendGetRequest();
+    void sendPostRequest();
+    void sendPutRequest();
+    void onRequestFinished(QNetworkReply* reply);
 };
 
 #endif // APITESTER_H
