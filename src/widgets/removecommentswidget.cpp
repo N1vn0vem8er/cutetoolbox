b70c511b87e590577f70d32a5ca773ba8a356e2a
N1vn0vem8er <fq0xveupd5d@outlook.com>
2025-12-17 18:50:02 +0100

    Implemented setting font removecommentswidget

--- src/widgets/removecommentswidget.cpp
+++ src/widgets/removecommentswidget.cpp
@@ -1,6 +1,9 @@
 #include "removecommentswidget.h"
 #include "src/widgets/ui_removecommentswidget.h"
 
+#include <QFontDialog>
+#include <qinputdialog.h>
+
 RemoveCommentsWidget::RemoveCommentsWidget(QWidget *parent)
     : CustomWidget(parent)
     , ui(new Ui::RemoveCommentsWidget)
@@ -11,4 +14,97 @@ RemoveCommentsWidget::RemoveCommentsWidget(QWidget *parent)
 RemoveCommentsWidget::~RemoveCommentsWidget()
 {
     delete ui;
+}
+
+bool RemoveCommentsWidget::canOpenFiles() const
+{
+    return true;
+}
+
+bool RemoveCommentsWidget::canSaveFiles() const
+{
+    return true;
+}
+
+bool RemoveCommentsWidget::canBasicEdit() const
+{
+    return true;
+}
+
+bool RemoveCommentsWidget::canChangeFont() const
+{
+    return true;
+}
+
+void RemoveCommentsWidget::save()
+{
+
+}
+
+void RemoveCommentsWidget::saveAs()
+{
+
+}
+
+void RemoveCommentsWidget::open()
+{
+
+}
+
+void RemoveCommentsWidget::close()
+{
+
+}
+
+void RemoveCommentsWidget::increaseFontSize()
+{
+    ui->codeEditor->increaseFontSize();
+}
+
+void RemoveCommentsWidget::decreaseFontSize()
+{
+    ui->codeEditor->decreaseFontSize();
+}
+
+void RemoveCommentsWidget::setFontSize()
+{
+    bool ok;
+    const int size = QInputDialog::getInt(this, tr("Set font size"), tr("Font size"), 1, 1, 200, 1, &ok);
+    if(ok)
+        ui->codeEditor->setFontSize(size);
+}
+
+void RemoveCommentsWidget::resetFontSize()
+{
+    ui->codeEditor->setFontSize(10);
+}
+
+void RemoveCommentsWidget::setFont()
+{
+    bool ok;
+    const QFont font = QFontDialog::getFont(&ok, this);
+    if(ok)
+    {
+        ui->codeEditor->setFont(font);
+    }
+}
+
+QString RemoveCommentsWidget::getOpenedFileName() const
+{
+
+}
+
+QStringList RemoveCommentsWidget::getRecentFiles() const
+{
+
+}
+
+void RemoveCommentsWidget::openFromRecent(const QString &path)
+{
+
+}
+
+void RemoveCommentsWidget::clearRecent()
+{
+
 }
--- src/widgets/removecommentswidget.h
+++ src/widgets/removecommentswidget.h
@@ -15,6 +15,23 @@ class RemoveCommentsWidget : public CustomWidget
 public:
     explicit RemoveCommentsWidget(QWidget *parent = nullptr);
     ~RemoveCommentsWidget();
+    bool canOpenFiles() const override;
+    bool canSaveFiles() const override;
+    bool canBasicEdit() const override;
+    bool canChangeFont() const override;
+    void save() override;
+    void saveAs() override;
+    void open() override;
+    void close() override;
+    void increaseFontSize() override;
+    void decreaseFontSize() override;
+    void setFontSize() override;
+    void resetFontSize() override;
+    void setFont() override;
+    QString getOpenedFileName() const override;
+    QStringList getRecentFiles() const override;
+    void openFromRecent(const QString &path) override;
+    void clearRecent() override;
 
 private:
     Ui::RemoveCommentsWidget *ui;
